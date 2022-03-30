54815e71d35abcefcb5c5c2633e437259751f968
Ryan M. Moore <rmm1047@gmail.com>
2022-03-29 21:14:55 -0400

    Update and add tests

--- bin/gen_multi.ml
+++ bin/gen_multi.ml
@@ -1,17 +1,30 @@
 open! Base
 
+let ok_or_abort x =
+  Or_error.iter_error x ~f:(fun err ->
+      Bin_utils.exit ~code:1 @@ Error.to_string_hum err)
+
+(* TODO just fix the run function so it won't raise... *)
+let run_wrapper opts =
+  Or_error.join @@ Or_error.try_with (fun () -> Bin_utils.run opts)
+
 let fname = (Sys.get_argv ()).(1)
 
 let opts =
   let open Stdio in
-  In_channel.with_file fname ~f:(fun ic ->
-      List.rev @@ snd
-      @@ In_channel.fold_lines ic ~init:(0, []) ~f:(fun (i, things) line ->
-             if i > 0 then
-               (i + 1, Or_error.ok_exn (Cli.opts_of_string line) :: things)
-             else (i + 1, things)))
+  let opts =
+    In_channel.with_file fname ~f:(fun ic ->
+        Or_error.all @@ List.rev @@ snd
+        @@ In_channel.fold_lines ic ~init:(0, []) ~f:(fun (i, things) line ->
+               if i > 0 then (i + 1, Cli.opts_of_string line :: things)
+               else (i + 1, things)))
+  in
+  let open Or_error.Let_syntax in
+  match%bind opts with
+  | [] -> Or_error.error_string "Got no options"
+  | opts -> Or_error.return opts
 
 let () =
-  match Or_error.all_unit @@ List.map opts ~f:Bin_utils.run with
-  | Ok () -> ()
-  | Error error -> Bin_utils.exit ~code:1 @@ Error.to_string_hum error
+  match opts with
+  | Ok opts -> ok_or_abort @@ Or_error.all_unit @@ List.map opts ~f:run_wrapper
+  | Error err -> Bin_utils.exit ~code:1 (Error.to_string_hum err)
---
+++ test/gen_multi.t/run.t
@@ -0,0 +1,22 @@
+Order of tsv file matters.
+
+  $ gen_multi specs/cli_specs.tsv | grep '^module'
+  module Human : sig
+  module Cat : sig
+
+  $ gen_multi specs/cli_specs_cat_first.tsv | grep '^module'
+  module Cat : sig
+  module Human : sig
+
+Relative filenames will be relative to where the command is run, not where the
+file is.
+
+  $ gen_multi specs/bad/bad.tsv
+  (Sys_error "../cat_spec.txt: No such file or directory")
+  [1]
+
+Errors
+
+  $ gen_multi bad.tsv
+  (Failure "bad config line: is good")
+  [1]
